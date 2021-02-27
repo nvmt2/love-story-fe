6261d597a9916a3b3f575bf276754b719a4a582a
minhtoi <minhtoi2799@gmail.com>
2021-02-27 22:18:36 +0700

    refactor code and adjust audio

--- js/playAudio.js
+++ js/playAudio.js
@@ -1,260 +1,31 @@
-//create animation of key
+// create box-shadow effect
 $.value = '0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22)';
 $(document).ready(function () {
-  //set shadow the key
   $(window).keypress(function (e) {
-    switch (e.originalEvent.key) {
-      case 'v': {
-        $('#white-key-c4').css('box-shadow', $.value);
-        break;
-      }
-      case '`': {
-        $('#white-key-d4').css('box-shadow', $.value);
-        var audio = new Audio('./assets/c4/Piano.ff.C4.mp3');
-        audio.play();
-        break;
-      }
-      case '9': {
-        $('#black-key-d-4').css('box-shadow', $.value);
-        break;
-      }
-      case 'o': {
-        $('#white-key-e4').css('box-shadow', $.value);
-        break;
-      }
-      case '0': {
-        $('#black-key-e-4').css('box-shadow', $.value);
-        break;
-      }
-      case 'p': {
-        $('#white-key-f4').css('box-shadow', $.value);
-        break;
-      }
-      case 'z': {
-        $('#white-key-g4').css('box-shadow', $.value);
-        break;
-      }
-      case 's': {
-        $('#black-key-g-4').css('box-shadow', $.value);
-        break;
-      }
-      case 'x': {
-        $('#white-key-a4').css('box-shadow', $.value);
-        break;
-      }
-      case 'd': {
-        $('#black-key-a-4').css('box-shadow', $.value);
-        break;
-      }
-      case 'c': {
-        $('#white-key-b4').css('box-shadow', $.value);
-        break;
-      }
-      case 'f': {
-        $('#black-key-b-4').css('box-shadow', $.value);
-        break;
-      }
-      //================================= tone 3 ==============================
-      //key Q
-      case 'q': {
-        var audio = new Audio('./assets/c3/Piano.ff.C3.mp3');
-        audio.play();
-        break;
-      }
-      //key W
-      case 'w': {
-        var audio = new Audio('./assets/c3/Piano.ff.D3.mp3');
-        audio.play();
-        break;
-      }
-      //key E
-      case 'e': {
-        var audio = new Audio('./assets/c3/Piano.ff.E3.mp3');
-        audio.play();
-        break;
-      }
-      //key R
-      case 'r': {
-        var audio = new Audio('./assets/c3/Piano.ff.F3.mp3');
-        audio.play();
-        break;
-      }
-      //key T
-      case 't': {
-        var audio = new Audio('./assets/c3/Piano.ff.G3.mp3');
-        audio.play();
-        break;
-      }
-      //key Y
-      case 'y': {
-        var audio = new Audio('./assets/c3/Piano.ff.A3.mp3');
-        audio.play();
-        break;
-      }
-      //key U
-      case 'u': {
-        var audio = new Audio('./assets/c3/Piano.ff.B3.mp3');
-        audio.play();
-        break;
-      }
-      //key 2
-      case '2': {
-        var audio = new Audio('./assets/c3/Piano.ff.Db3.mp3');
-        audio.play();
-        break;
-      }
-      //key 3
-      case '3': {
-        var audio = new Audio('./assets/c3/Piano.ff.Eb3.mp3');
-        audio.play();
-        break;
-      }
-      //key 5
-      case '5': {
-        var audio = new Audio('./assets/c3/Piano.ff.Gb3-error.mp3');
-        audio.play();
-        break;
-      }
-      //key 6
-      case '6': {
-        var audio = new Audio('./assets/c3/Piano.ff.Ab3.mp3');
-        audio.play();
-        break;
-      }
-      //key 7
-      case '7': {
-        var audio = new Audio('./assets/c3/Piano.ff.Bb3.mp3');
-        audio.play();
-        break;
-      }
-      // ============================================ tone 5 ===========================================
-      //key B
-      case 'b': {
-        var audio = new Audio('./assets/c5/Piano.ff.C5.mp3');
-        audio.play();
-        break;
-      }
-      //key N
-      case 'n': {
-        var audio = new Audio('./assets/c5/Piano.ff.D5.mp3');
-        audio.play();
-        break;
-      }
-      //key M
-      case 'm': {
-        var audio = new Audio('./assets/c5/Piano.ff.E5.mp3');
-        audio.play();
-        break;
-      }
-      //key ,
-      case ',': {
-        var audio = new Audio('./assets/c5/Piano.ff.F5.mp3');
-        audio.play();
-        break;
-      }
-      //key .
-      case '.': {
-        var audio = new Audio('./assets/c5/Piano.ff.G5.mp3');
-        audio.play();
-        break;
-      }
-      //key /
-      case '/': {
-        var audio = new Audio('./assets/c5/Piano.ff.A5.mp3');
-        audio.play();
-        break;
-      }
-      //key \
-      case '\\': {
-        // Âm thanh bị nhiễu
-        var audio = new Audio('./assets/c5/Piano.ff.B5.mp3');
-        audio.play();
-        break;
-      }
-      //key H
-      case 'h': {
-        var audio = new Audio('./assets/c5/Piano.ff.Db5.mp3');
-        audio.play();
-        break;
-      }
-      //key J
-      case 'j': {
-        var audio = new Audio('./assets/c5/Piano.ff.Eb5.mp3');
-        audio.play();
-        break;
-      }
-      //key L
-      case 'l': {
-        var audio = new Audio('./assets/c5/Piano.ff.Gb5.mp3');
-        audio.play();
-        break;
-      }
-      //key ;
-      case ';': {
-        var audio = new Audio('./assets/c5/Piano.ff.Ab5.mp3');
-        audio.play();
-        break;
-      }
-      //key '
-      case "'": {
-        var audio = new Audio('./assets/c5/Piano.ff.Bb5.mp3');
-        audio.play();
-        break;
-      }
+    // get key pressed by user
+    let keyPressed = e.originalEvent.key;
 
-      default: {
+    for (let item of storeTone) {
+      if (item.key === keyPressed) {
+        // Look for corresponding audio
+        let myAudio = new Audio(item.path);
+        // run audio
+        myAudio.play();
+        //set shadow the key
+        $(item.id).css('box-shadow', $.value);
+        break;
       }
     }
   });
   //set the normal key
   $(window).keyup(function (e) {
-    switch (e.originalEvent.key) {
-      case 'v': {
-        $('#white-key-c4').css('box-shadow', 'none');
-        break;
-      }
-      case 'i': {
-        $('#white-key-d4').css('box-shadow', 'none');
-        break;
-      }
-      case '9': {
-        $('#black-key-d-4').css('box-shadow', 'none');
-        break;
-      }
-      case 'o': {
-        $('#white-key-e4').css('box-shadow', 'none');
-        break;
-      }
-      case '0': {
-        $('#white-key-f4').css('box-shadow', 'none');
-        break;
-      }
-      case 'p': {
-        $('#black-key-e-4').css('box-shadow', 'none');
-        break;
-      }
-      case 'z': {
-        $('#white-key-g4').css('box-shadow', 'none');
-        break;
-      }
-      case 's': {
-        $('#black-key-g-4').css('box-shadow', 'none');
-        break;
-      }
-      case 'x': {
-        $('#white-key-a4').css('box-shadow', 'none');
-        break;
-      }
-      case 'd': {
-        $('#black-key-a-4').css('box-shadow', 'none');
-        break;
-      }
-      case 'c': {
-        $('#white-key-b4').css('box-shadow', 'none');
-        break;
-      }
-      case 'f': {
-        $('#black-key-b-4').css('box-shadow', 'none');
+    // get key pressed by user
+    let keyPressed = e.originalEvent.key;
+
+    for (let item of storeTone) {
+      if (item.key === keyPressed) {
+        // turn off box-shadow effect
+        $(item.id).css('box-shadow', 'none');
         break;
       }
     }
---
+++ js/storeAudio.js
@@ -0,0 +1,186 @@
+let baseUrlTone = './assets/audio-piano';
+let storeTone = [
+  // tone 3
+  {
+    id: '#note-c3',
+    key: 'q',
+    path: `${baseUrlTone}/c3/Piano.ff.C3.mp3`,
+  },
+  {
+    id: '#note-d3',
+    key: 'w',
+    path: `${baseUrlTone}/c3/Piano.ff.D3.mp3`,
+  },
+  {
+    id: '#note-e3',
+    key: 'e',
+    path: `${baseUrlTone}/c3/Piano.ff.E3.mp3`,
+  },
+  {
+    id: '#note-f3',
+    key: 'r',
+    path: `${baseUrlTone}/c3/Piano.ff.F3.mp3`,
+  },
+  {
+    id: '#note-g3',
+    key: 't',
+    path: `${baseUrlTone}/c3/Piano.ff.G3.mp3`,
+  },
+  {
+    id: '#note-a3',
+    key: 'y',
+    path: `${baseUrlTone}/c3/Piano.ff.A3.mp3`,
+  },
+  {
+    id: '#note-b3',
+    key: 'u',
+    path: `${baseUrlTone}/c3/Piano.ff.B3.mp3`,
+  },
+  {
+    id: '#note-db3',
+    key: '2',
+    path: `${baseUrlTone}/c3/Piano.ff.Db3.mp3`,
+  },
+  {
+    id: '#note-eb3',
+    key: '3',
+    path: `${baseUrlTone}/c3/Piano.ff.Eb3.mp3`,
+  },
+  {
+    id: '#note-gb3',
+    key: '5',
+    path: `${baseUrlTone}/c3/Piano.ff.Gb3.mp3`,
+  },
+  {
+    id: '#note-ab3',
+    key: '6',
+    path: `${baseUrlTone}/c3/Piano.ff.Ab3.mp3`,
+  },
+  {
+    id: '#note-bb3',
+    key: '7',
+    path: `${baseUrlTone}/c3/Piano.ff.Bb3.mp3`,
+  },
+  // tone 4
+  {
+    id: '#note-c4',
+    key: 'i',
+    path: `${baseUrlTone}/c4/Piano.ff.C4.mp3`,
+  },
+  {
+    id: '#note-d4',
+    key: 'o',
+    path: `${baseUrlTone}/c4/Piano.ff.D4.mp3`,
+  },
+  {
+    id: '#note-e4',
+    key: 'p',
+    path: `${baseUrlTone}/c4/Piano.ff.E4.mp3`,
+  },
+  {
+    id: '#note-f4',
+    key: 'z',
+    path: `${baseUrlTone}/c4/Piano.ff.F4.mp3`,
+  },
+  {
+    id: '#note-g4',
+    key: 'x',
+    path: `${baseUrlTone}/c4/Piano.ff.G4.mp3`,
+  },
+  {
+    id: '#note-a4',
+    key: 'c',
+    path: `${baseUrlTone}/c4/Piano.ff.A4.mp3`,
+  },
+  {
+    id: '#note-b4',
+    key: 'v',
+    path: `${baseUrlTone}/c4/Piano.ff.B4.mp3`,
+  },
+  {
+    id: '#note-db4',
+    key: '9',
+    path: `${baseUrlTone}/c4/Piano.ff.Db4.mp3`,
+  },
+  {
+    id: '#note-eb4',
+    key: '0',
+    path: `${baseUrlTone}/c4/Piano.ff.Eb4.mp3`,
+  },
+  {
+    id: '#note-gb4',
+    key: 's',
+    path: `${baseUrlTone}/c4/Piano.ff.Gb4.mp3`,
+  },
+  {
+    id: '#note-ab4',
+    key: 'd',
+    path: `${baseUrlTone}/c4/Piano.ff.Ab4.mp3`,
+  },
+  {
+    id: '#note-bb4',
+    key: 'f',
+    path: `${baseUrlTone}/c4/Piano.ff.Bb4.mp3`,
+  },
+  // tone 5
+  {
+    id: '#note-c5',
+    key: 'b',
+    path: `${baseUrlTone}/c5/Piano.ff.C5.mp3`,
+  },
+  {
+    id: '#note-d5',
+    key: 'n',
+    path: `${baseUrlTone}/c5/Piano.ff.D5.mp3`,
+  },
+  {
+    id: '#note-e5',
+    key: 'm',
+    path: `${baseUrlTone}/c5/Piano.ff.E5.mp3`,
+  },
+  {
+    id: '#note-f5',
+    key: ',',
+    path: `${baseUrlTone}/c5/Piano.ff.F5.mp3`,
+  },
+  {
+    id: '#note-g5',
+    key: '.',
+    path: `${baseUrlTone}/c5/Piano.ff.G5.mp3`,
+  },
+  {
+    id: '#note-a5',
+    key: '/',
+    path: `${baseUrlTone}/c5/Piano.ff.A5.mp3`,
+  },
+  {
+    id: '#note-b5',
+    key: '\\',
+    path: `${baseUrlTone}/c5/Piano.ff.B5.mp3`,
+  },
+  {
+    id: '#note-db5',
+    key: 'h',
+    path: `${baseUrlTone}/c5/Piano.ff.Db5.mp3`,
+  },
+  {
+    id: '#note-eb5',
+    key: 'j',
+    path: `${baseUrlTone}/c5/Piano.ff.Eb5.mp3`,
+  },
+  {
+    id: '#note-gb5',
+    key: 'l',
+    path: `${baseUrlTone}/c5/Piano.ff.Gb5.mp3`,
+  },
+  {
+    id: '#note-ab5',
+    key: ';',
+    path: `${baseUrlTone}/c5/Piano.ff.Ab5.mp3`,
+  },
+  {
+    id: '#note-bb5',
+    key: "'",
+    path: `${baseUrlTone}/c5/Piano.ff.Bb5.mp3`,
+  },
+];
